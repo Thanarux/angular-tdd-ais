a080b0a493a9869a9f04bdcfae2b69743c0a7ecd
Weera Kasetsin <wkasetsin@gmail.com>
2015-08-02 10:47:39 +0700

    Use  to mock request

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 angular.module('Contactical', [])
-	.service('ContactService', function(){
+	.service('ContactService', function($http){
 		var vm = this;
 		vm.contacts = [];
+		$http.get('http://localhost:9001/contacts');
 	});
--- test/main.spec.js
+++ test/main.spec.js
@@ -1,14 +1,25 @@
 describe('Contactical', function() {
 	describe('ContactService', function() {
 		var service;
+		var $httpBackend;
+
 		beforeEach(function(){
 			module('Contactical');
 			inject(function($injector){
 				service = $injector.get('ContactService');
+				$httpBackend = $injector.get('$httpBackend');
 			});
 		})
+
 		it('should contain data in array', function(){
 			expect(service.contacts).toEqual(jasmine.any(Array));
-		});
+		})
+
+		it('should call backend', function(){
+			$httpBackend.expectGET('http://localhost:9001/contacts')
+				.respond(200, []);
+
+			$httpBackend.flush();
+		})
 	});
 });
