b72075b7897b2081361c314627a7731d37e49df7
Weera Kasetsin <wkasetsin@gmail.com>
2015-08-02 11:05:41 +0700

    Add spec for test 'ListContacts' controller

--- app/main.js
+++ app/main.js
@@ -2,5 +2,14 @@ angular.module('Contactical', [])
 	.service('ContactService', function($http){
 		var vm = this;
 		vm.contacts = [];
-		$http.get('http://localhost:9001/contacts');
+		$http.get('http://localhost:9001/contacts')
+			.then(function(response){
+				while(response.data[0]) {
+					vm.contacts.push(response.data.pop());
+				}
+			});
+	})
+	.controller('ListContacts', function($scope, ContactService){
+		var vm = this;
+		$scope.contacts = ContactService.contacts;
 	});
--- test/main.spec.js
+++ test/main.spec.js
@@ -22,4 +22,26 @@ describe('Contactical', function() {
 			$httpBackend.flush();
 		})
 	});
+
+	describe('ListContacts', function() {
+		var $scope;
+		var contactService;
+		var $httpBackend;
+		var $controller;
+
+		beforeEach(function(){
+			module('Contactical');
+			inject(function($injector, $rootScope){
+				$scope = $rootScope.$new();
+				contactService = $injector.get('ContactService');
+				$httpBackend = $injector.get('$httpBackend');
+				$controller = $injector.get('$controller');
+			});
+		});
+
+		it('should store an array of contacts in scope', function(){
+			$controller('ListContacts', { $scope: $scope, ContactService: contactService });
+			expect($scope.contacts).toEqual(jasmine.any(Array));
+		});
+	});
 });
